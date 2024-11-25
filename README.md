3823a5d7c936c0d1b39b91fd215d8238f75f4e06
Mahmoud-ghareeb <mahmoudghareeb11111@gmail.com>
2024-11-25 03:25:54 +0300

    uploading assistant

---
+++ README.md
@@ -0,0 +1,31 @@
+# Assistant
+
+### Installation Guide
+
+1. Clone the repo
+```shell
+git clone https://github.com/Mahmoud-ghareeb/Assistant.git
+``` 
+
+1. Create Python Environment
+```shell
+conda create --name assistant python==3.12
+```
+
+2. Activate the environment
+```shell
+conda activate assistant
+```
+
+3. install the requirments
+```shell
+pip install -r requirements.txt
+```
+
+4. Rename .env.example to .env and add the secret keys of 
+    - openai
+
+5. Start the app
+```shell
+uvicorn main:app --port 8080
+```
---
+++ src/assistant.py
@@ -0,0 +1,94 @@
+from src.memory import create_memory, create_context, create_history
+from src.embedding import load_and_embedd, encode
+from src.args import Args
+
+from langchain.prompts import PromptTemplate
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class Assistant:
+
+    def __init__(self):
+        self.g_vars = {}
+        self.client = OpenAI(api_key=os.getenv('OPEN_AI_API_KEY'))
+        self.memo = []
+
+    def get_the_prompt(self, question):
+        results = self.g_vars['dp'].max_marginal_relevance_search(
+            question, k=1)
+        template = self.g_vars['config']['prompts']['course_propmt']
+        context = create_context(results)
+        history = create_history(self.g_vars['memory'].chat_memory.messages)
+
+        new_template = template.format(
+            context=context,
+            history=history,
+            question=question
+        )
+        prompt = PromptTemplate(
+            input_variables=["context", "history", "question"],
+            template=new_template
+        )
+
+        return prompt
+
+    def stream_text(self, question):
+        prompt = self.get_the_prompt(question)
+        return self.openai_llm(prompt, question)
+
+    def openai_llm_with_memmory(self, prompt, question):
+
+        messages = [
+            {"role": "system", "content": prompt.template}
+        ]
+
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            temperature=0,
+            messages=messages,
+            stream=True,
+        )
+        
+        res = ""
+
+        for chunk in response:
+            txt = chunk.choices[0].delta.content
+            if txt:
+                res += txt
+                self.memo.append(txt)
+
+                yield txt
+
+        self.g_vars['memory'].chat_memory.messages = []
+        self.g_vars['memory'].save_context(
+            {"input": question}, {"output": res})
+
+    def openai_llm(self, prompt, question):
+
+        messages = [
+            {"role": "system", "content": prompt.template}
+        ]
+
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            temperature=0,
+            messages=messages,
+            stream=True,
+        )
+
+        res = ""
+
+        for chunk in response:
+            txt = chunk.choices[0].delta.content
+            if txt:
+                res += txt
+
+                yield txt
+
+        self.g_vars['memory'].chat_memory.messages = []
+        self.g_vars['memory'].save_context(
+            {"input": question}, {"output": res})
